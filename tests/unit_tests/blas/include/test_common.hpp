7c03871911995ec3dd35a9ea0af6cfe37af81906
sbalint98 <sbsbalint14@gmail.com>
2021-06-02 18:48:50 +0200

    Remove useses of ifdefs from the tests

--- tests/unit_tests/blas/include/test_common.hpp
+++ tests/unit_tests/blas/include/test_common.hpp
@@ -30,14 +30,14 @@
 #include "oneapi/mkl/detail/config.hpp"
 
 namespace std {
-#ifdef ENABLE_HALF_ROUTINES
+//#ifdef ENABLE_HALF_ROUTINES
 static cl::sycl::half abs(cl::sycl::half v) {
     if (v < cl::sycl::half(0))
         return -v;
     else
         return v;
 }
-#endif
+//#endif
 } // namespace std
 
 // Complex helpers.
@@ -143,12 +143,12 @@ template <>
 uint8_t rand_scalar() {
     return std::rand() % 128;
 }
-#ifdef ENABLE_HALF_ROUTINES
+//#ifdef ENABLE_HALF_ROUTINES
 template <>
-half rand_scalar() {
-    return half(std::rand() % 32000) / half(32000) - half(0.5);
+cl::sycl::half rand_scalar() {
+    return cl::sycl::half(std::rand() % 32000) / cl::sycl::half(32000) - cl::sycl::half(0.5);
 }
-#endif
+//#endif
 template <typename fp>
 static fp rand_scalar(int mag) {
     fp tmp = fp(mag) + fp(std::rand()) / fp(RAND_MAX) - fp(0.5);
--- tests/unit_tests/blas/level3/gemm.cpp
+++ tests/unit_tests/blas/level3/gemm.cpp
@@ -144,41 +144,39 @@ int test(device* dev, oneapi::mkl::layout layout, oneapi::mkl::transpose transa,
 
 class GemmTests
         : public ::testing::TestWithParam<std::tuple<cl::sycl::device*, oneapi::mkl::layout>> {};
-#ifdef ENABLE_HALF_ROUTINES
 TEST_P(GemmTests, HalfHalfFloatPrecision) {
     float alpha(2.0);
     float beta(3.0);
-    EXPECT_TRUEORSKIP((test<half, float>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, float>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::nontrans,
         oneapi::mkl::transpose::nontrans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, float>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, float>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::nontrans,
         oneapi::mkl::transpose::trans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, float>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, float>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::trans,
         oneapi::mkl::transpose::nontrans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, float>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, float>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::trans,
         oneapi::mkl::transpose::trans, 79, 83, 91, 103, 105, 106, alpha, beta)));
 }
 
 TEST_P(GemmTests, RealHalfPrecision) {
-    half alpha(2.0);
-    half beta(3.0);
-    EXPECT_TRUEORSKIP((test<half, half>(
+    cl::sycl::half alpha(2.0);
+    cl::sycl::half beta(3.0);
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, cl::sycl::half>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::nontrans,
         oneapi::mkl::transpose::nontrans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, half>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, cl::sycl::half>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::nontrans,
         oneapi::mkl::transpose::trans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, half>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, cl::sycl::half>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::trans,
         oneapi::mkl::transpose::nontrans, 79, 83, 91, 103, 105, 106, alpha, beta)));
-    EXPECT_TRUEORSKIP((test<half, half>(
+    EXPECT_TRUEORSKIP((test<cl::sycl::half, cl::sycl::half>(
         std::get<0>(GetParam()), std::get<1>(GetParam()), oneapi::mkl::transpose::trans,
         oneapi::mkl::transpose::trans, 79, 83, 91, 103, 105, 106, alpha, beta)));
 }
-#endif
 TEST_P(GemmTests, RealSinglePrecision) {
     float alpha(2.0);
     float beta(3.0);
